95c04a39f5edb0338ef71b051e0783640bf72a07
Hector Martinez Espinosa <mtzehector@gmail.com>
2025-10-31 21:34:53 -0600

    Actualizaci├│n inicial del proyecto

--- src/main/java/mx/gob/imss/dpes/entidadfinancierafront/model/Tramite.java
+++
@@ -1,9 +0,0 @@
-package mx.gob.imss.dpes.entidadfinancierafront.model;
-
-import lombok.Data;
-import mx.gob.imss.dpes.common.model.BaseModel;
-
-@Data
-public class Tramite extends BaseModel {
-    private String folioTramite;
-}
